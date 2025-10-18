7ba22d0462d36dd701e00961e4d80d3ad2c67d91
Matheus Torres <30413727+matttheust@users.noreply.github.com>
2025-10-18 06:19:37 -0300

    Initial commit

---
+++ MotoLog/MotoLog/App/MotoLogApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotoLogApp.swift
+//  MotoLog
+//
+//  Created by Matheus Torres on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MotoLogApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MotoLog/MotoLog/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MotoLog
+//
+//  Created by Matheus Torres on 18/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
